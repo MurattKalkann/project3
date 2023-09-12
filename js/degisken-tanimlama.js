c2c2c90ff8d73e23f0aa04eea9f93280dec4fe6a
Murat <muratkalkan1918@gmail.com>
2023-09-12 15:12:54 +0300

    js dosyasi eklendi

---
+++ js/degisken-tanimlama.js
@@ -0,0 +1,12 @@
+//let ile degiskeni bos tanimlamak
+let serverName = "api.kodluyoruz.org"
+console.log(serverName);
+//let ile degiskene bilgi atamak
+//serverName = "kodluyoruz.org"
+//console.log(serverName);
+//let ile degiskene bilgi atayarak tanimlamak
+//let password ="1234"
+//console.log(password);
+//degisken atamasi yapmadan once kullanmaya calismak
+//console.log(fullName);
+//let fullName="Murat Kalkan"
